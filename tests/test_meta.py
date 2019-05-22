76c2682da884816a68ac860a1eec4890a16ca75a
Jason R. Coombs <jaraco@jaraco.com>
2019-05-22 12:15:43 -0400

    Mark test as xfail on Python 2.7 due to pep517 being a flit package.

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -1,8 +1,14 @@
 import re
 
+import pytest
+
 from pep517 import meta
 
 
+@pytest.mark.xfail(
+    'sys.version_info < (3,)',
+    reason="pep517 cannot be built on Python 2",
+)
 def test_meta_for_this_package():
     dist = meta.load('.')
     assert re.match(r'[\d.]+', dist.version)
