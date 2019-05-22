6d06c6a51c52368d4077c5149f530d6b6de70909
Jason R. Coombs <jaraco@jaraco.com>
2019-05-22 12:32:05 -0400

    Add another test demonstrating support for classic packages.

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals, absolute_import, division
+
 import re
 
 import pytest
@@ -13,3 +15,13 @@ def test_meta_for_this_package():
     dist = meta.load('.')
     assert re.match(r'[\d.]+', dist.version)
     assert dist.metadata['Name'] == 'pep517'
+
+
+def test_classic_package(tmpdir):
+    (tmpdir / 'setup.py').write_text(
+        'from distutils.core import setup; setup(name="foo", version="1.0")',
+        encoding='utf-8',
+    )
+    dist = meta.load(str(tmpdir))
+    assert dist.version == '1.0'
+    assert dist.metadata['Name'] == 'foo'
