0c24fd73922cab4fe3fb605bc64e5fdc7b093637
Jason R. Coombs <jaraco@jaraco.com>
2019-05-22 12:06:53 -0400

    Add a basic test for the metadata functionality.

---
+++ tests/test_meta.py
@@ -0,0 +1,10 @@
+import re
+
+
+from pep517 import meta
+
+
+def test_meta_for_this_package():
+    dist = meta.load('.')
+    assert re.match(r'[\d.]+', dist.version)
+    assert dist.metadata['Name'] == 'pep517'
