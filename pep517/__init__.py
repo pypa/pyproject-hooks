edb27610ec723392eedd366c51a484a9f12ffa4e
Andrey Bienkowski <hexagon-recursion@posteo.net>
2022-04-10 11:08:01 +0300

    flake8

--- pep517/__init__.py
+++ pep517/__init__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2017-2021 Thomas Kluyver <thomas@kluyver.me.uk> and other contributors
+# SPDX-FileCopyrightText: 2017-2021 Thomas Kluyver <thomas@kluyver.me.uk> and other contributors  # noqa: E501
 #
 # SPDX-License-Identifier: MIT
 
