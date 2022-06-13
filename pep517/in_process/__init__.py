9e254968f67d3b0607141a64384bdee655193e12
Jason R. Coombs <jaraco@jaraco.com>
2022-06-12 23:18:38 -0400

    Use files API on Python 3.9 and later. Fixes #140.

--- pep517/in_process/__init__.py
+++ pep517/in_process/__init__.py
@@ -9,10 +9,18 @@ from os.path import join as pjoin
 
 try:
     import importlib.resources as resources
-
-    def _in_proc_script_path():
-        return resources.path(__package__, '_in_process.py')
+    try:
+        resources.files
+    except AttributeError:
+        # Python 3.8 compatibility
+        def _in_proc_script_path():
+            return resources.path(__package__, '_in_process.py')
+    else:
+        def _in_proc_script_path():
+            return resources.as_file(
+                resources.files(__package__).joinpath('_in_process.py'))
 except ImportError:
+    # Python 3.6 compatibility
     @contextmanager
     def _in_proc_script_path():
         yield pjoin(dirname(abspath(__file__)), '_in_process.py')
