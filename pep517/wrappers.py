6526b636b3749382e6d7700974acbf6884033d57
aws-taylor <57725958+aws-taylor@users.noreply.github.com>
2022-06-15 09:29:18 -0700

    Propagate 'message' in BackendInvalid
    Propagate the 'message' field up to the parent Exception class so that errors are displayed more clearly on the command line. Prior to this change, a user sees the following when a BackendInvalid exception is thrown by a build backend:
    
    ```
    > python3 -m pip install <some package>
    <traceback>
    pip._vendor.pep517.wrappers.BackendInvalid
    ```
    
    And after, the user will see:
    
    ```
    python3 -m pip install <some package>
    <traceback>
    pip._vendor.pep517.wrappers.BackendInvalid: <message>
    ```

--- pep517/wrappers.py
+++ pep517/wrappers.py
@@ -40,9 +40,9 @@ class BackendUnavailable(Exception):
 class BackendInvalid(Exception):
     """Will be raised if the backend is invalid."""
     def __init__(self, backend_name, backend_path, message):
+        super().__init__(message)
         self.backend_name = backend_name
         self.backend_path = backend_path
-        self.message = message
 
 
 class HookMissing(Exception):
