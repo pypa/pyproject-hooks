f97e7de07164e3ce38f08c64323511faad030d6e
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2022-10-29 08:57:15 -0500

    Drop support for Python 3.6
    This Python version is post-EOL.

--- src/pyproject_hooks/_in_process/__init__.py
+++ src/pyproject_hooks/_in_process/__init__.py
@@ -3,24 +3,16 @@
 The subpackage should stay as empty as possible to avoid shadowing modules that
 the backend might import.
 """
-from contextlib import contextmanager
-from os.path import abspath, dirname
-from os.path import join as pjoin
+
+import importlib.resources as resources
 
 try:
-    import importlib.resources as resources
-    try:
-        resources.files
-    except AttributeError:
-        # Python 3.8 compatibility
-        def _in_proc_script_path():
-            return resources.path(__package__, '_in_process.py')
-    else:
-        def _in_proc_script_path():
-            return resources.as_file(
-                resources.files(__package__).joinpath('_in_process.py'))
-except ImportError:
-    # Python 3.6 compatibility
-    @contextmanager
+    resources.files
+except AttributeError:
+    # Python 3.8 compatibility
     def _in_proc_script_path():
-        yield pjoin(dirname(abspath(__file__)), '_in_process.py')
+        return resources.path(__package__, '_in_process.py')
+else:
+    def _in_proc_script_path():
+        return resources.as_file(
+            resources.files(__package__).joinpath('_in_process.py'))
