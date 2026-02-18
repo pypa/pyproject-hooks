00ef33440bbcc5df14846b2943c380f36728dda0
Thomas Kluyver <thomas@kluyver.me.uk>
2026-02-18 12:56:50 +0100

    Improve check that backend is in source tree, remove use of commonprefix

--- src/pyproject_hooks/_impl.py
+++ src/pyproject_hooks/_impl.py
@@ -117,12 +117,9 @@ def norm_and_check(source_tree: str, requested: str) -> str:
 
     abs_source = os.path.abspath(source_tree)
     abs_requested = os.path.normpath(os.path.join(abs_source, requested))
-    # We have to use commonprefix for Python 2.7 compatibility. So we
-    # normalise case to avoid problems because commonprefix is a character
-    # based comparison :-(
     norm_source = os.path.normcase(abs_source)
     norm_requested = os.path.normcase(abs_requested)
-    if os.path.commonprefix([norm_source, norm_requested]) != norm_source:
+    if not norm_requested.startswith(norm_source + os.sep):
         raise ValueError("paths must be inside source tree")
 
     return abs_requested
